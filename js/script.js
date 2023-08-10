46a2e08d69361f30981f296a622f09d3964903b5
lajoc_devs <bolajifisayo80@gmail.com>
2023-08-10 05:59:36 +0100

    stopped at day

--- js/script.js
+++ js/script.js
@@ -12,15 +12,41 @@ let hoursEl = document.getElementById("hours");
 let minutesEl = document.getElementById("minutes");
 let secondsEl = document.getElementById("seconds");
 
+//to return month as name
+const months = [
+	"Jan",
+	"Feb",
+	"Mar",
+	"Apr",
+	"May",
+	"June",
+	"July",
+	"Aug",
+	"Sept",
+	"Oct",
+	"Nov",
+	"Dec",
+];
+//to return days as name
+const weekdays = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
+
 //DOM append
 currentTimeEl.innerHTML = currentDate.toLocaleTimeString();
   //time append
-hoursEl.innerHTML += ": " + currentDate.getHours();
-minutesEl.innerHTML += ": " + currentDate.getMinutes();
-secondsEl.innerHTML += ": " + currentDate.getSeconds();
+hoursEl.innerHTML += " " + currentDate.getHours();
+minutesEl.innerHTML += " " + currentDate.getMinutes();
+secondsEl.innerHTML += " " + currentDate.getSeconds();
 
   //date append
-currentYearEl.innerHTML += ": " + currentDate.getFullYear();
-currentMonthEl.innerHTML += ": " + currentDate.months[getFullYear()];
-currentDayEl.innerHTML += ": " + currentDate.getFullYear();
-currentWeekDayEl.innerHTML += ": " + currentDate.getFullYear();
+currentYearEl.innerHTML += " " + currentDate.getFullYear();
+currentMonthEl.innerHTML += " " + months[currentDate.getMonth()];
+currentDayEl.innerHTML += " " + weekdays[currentDate.getDay()];
+currentWeekDayEl.innerHTML += " " + currentDate.getFullYear();
