4ae9646cfc89bf82e9eef5f00831c39936ddd7bf
lajoc_devs <bolajifisayo80@gmail.com>
2023-08-10 05:38:44 +0100

    re-writing the deleted javascript when pc crashed

---
+++ js/script.js
@@ -0,0 +1,13 @@
+ const currentDate = new Date();
+// DOM fetch
+const currentTimeEl = document.getElementById("currentTime");
+  //date
+let currentYearEl = document.getElementById("currentYear");
+let currentMonthEl = document.getElementById("currentMonth");
+let currentDayEl = document.getElementById("currentDay");
+let currentWeekDayEl = document.getElementById("currentWeekDay");
+
+  //time
+let hoursEl = document.getElementById("hours");
+let minutesEl = document.getElementById("minutes");
+let secondsEl = document.getElementById("seconds");
